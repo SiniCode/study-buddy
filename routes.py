a1d5d7f0e97f3d67e8cde477944a270c15770989
Sini Arkko <sini.arkko@gmail.com>
2022-02-03 18:10:12 +0200

    add route to sign-up page

--- routes.py
+++ routes.py
@@ -5,3 +5,7 @@ from flask import render_template, request, redirect
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
