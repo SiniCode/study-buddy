aca2a30a78c4cf3a43cc4af9254af3f46e64f268
Sini Arkko <sini.arkko@gmail.com>
2022-02-18 20:12:45 +0200

    fix deletion

--- chat.py
+++ chat.py
@@ -44,6 +44,7 @@ def delete_question(question_id):
     sql = """UPDATE questions SET visible=0
              WHERE id=:id AND user_id=:user_id"""
     db.session.execute(sql, {"id":question_id, "user_id":user_id})
+    db.session.commit()
     return True
 
 def delete_answer(answer_id):
@@ -53,4 +54,5 @@ def delete_answer(answer_id):
     sql = """UPDATE answers SET visible=0
              WHERE id=:id AND user_id=:user_id"""
     db.session.execute(sql, {"id":answer_id, "user_id":user_id})
+    db.session.commit()
     return True
